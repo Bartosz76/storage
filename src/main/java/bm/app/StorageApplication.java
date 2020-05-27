4458c0e21229354b2cd460f447dc5c6fe92329fc
Bartosz <mierzwinski.b@gmail.com>
2020-05-27 21:15:42 +0200

    added directory creating method and exceptions handling

--- src/main/java/bm/app/StorageApplication.java
+++ src/main/java/bm/app/StorageApplication.java
@@ -2,11 +2,15 @@ package bm.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:config.properties")
 @SpringBootApplication
 public class StorageApplication {
     public static void main(String[] args) {
         SpringApplication.run(StorageApplication.class);
 
     }
+
+
 }
--- src/main/java/bm/app/service/LocalFileService.java
+++ src/main/java/bm/app/service/LocalFileService.java
@@ -1,12 +1,50 @@
 package bm.app.service;
 
+import bm.app.exceptions.ConfigPropertyException;
+import org.apache.logging.log4j.util.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Service
 public class LocalFileService {
 
-    public static final Logger logger = LoggerFactory.getLogger(LocalFileService.class);
+    private static final Logger logger = LoggerFactory.getLogger(LocalFileService.class);
+
+    //@Value("${files.path}")
+    private String uploads = "C:\\Users\\User\\Desktop\\Ascension\\appFiles\\storageFiles\\";
+
+    public LocalFileService() {
+        try {
+            createContextDirectory();
+        } catch (ConfigPropertyException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createContextDirectory() throws ConfigPropertyException, IOException {
+        if (Strings.isBlank(this.uploads)) {
+            logger.error("Cannot get the path to files");
+            throw new ConfigPropertyException("Cannot get the path to files");
+        }
+
+        Path path = Paths.get(uploads);
+        if (Files.notExists(path)) {
+            try {
+                logger.info("Try to create a directory: {}", path);
+                Files.createDirectories(path);
+            } catch (IOException e) {
+                logger.error("Cannot create the directory: {}. Exception: {}", path, e.getMessage());
+                throw new IOException(e.getMessage());
+            }
+        }
+    }
+
 
 }
