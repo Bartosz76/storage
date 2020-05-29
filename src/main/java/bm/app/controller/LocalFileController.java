dca65ffcc0d04b530b9b8741f7523c5e67a49635
Bartosz <mierzwinski.b@gmail.com>
2020-05-29 03:39:54 +0200

    added RestController

--- src/main/java/bm/app/controller/LocalFileController.java
+++ src/main/java/bm/app/controller/LocalFileController.java
@@ -1,4 +1,25 @@
 package bm.app.controller;
 
+import bm.app.model.LocalFile;
+import bm.app.service.LocalFileService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
 public class LocalFileController {
+
+    private LocalFileService localFileService;
+
+    public LocalFileController(LocalFileService localFileService){
+        this.localFileService = localFileService;
+    }
+
+    @GetMapping("/files")
+    public List<LocalFile> getFiles(){
+        return localFileService.getFiles();
+    }
 }
--- src/main/java/bm/app/service/LocalFileService.java
+++ src/main/java/bm/app/service/LocalFileService.java
@@ -1,16 +1,27 @@
 package bm.app.service;
 
 import bm.app.exceptions.ConfigPropertyException;
+import bm.app.model.LocalFile;
 import org.apache.logging.log4j.util.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 public class LocalFileService {
@@ -46,5 +57,54 @@ public class LocalFileService {
         }
     }
 
+    public ResponseEntity<?> getFile(String fileName){
+        Resource resource;
+        Path path = Paths.get(uploads);
+        try {
+            resource = new UrlResource(path.toUri());
+        }catch (MalformedURLException e){
+            logger.error("Cannot get the file: {}", e.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .build();
+        }
 
+        File targetFile;
+        try {
+            targetFile = resource.getFile();
+        }catch (IOException e){
+            e.printStackTrace();
+            logger.error("Cannot get the file: {}", e.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .build();
+        }
+
+        String contentType;
+        try {
+            contentType = Files.probeContentType(path);
+        }catch (IOException e){
+            logger.error("Cannot get the file: {}", e.getMessage());
+            return ResponseEntity
+                    .ok()
+                    .body(e.getMessage());
+        }
+
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachement;filename=\"" + targetFile.getName() + "\"")
+                .contentLength(targetFile.length())
+                .body(resource);
+    }
+
+
+    public List<LocalFile> getFiles() {
+        Stream<Path> files;
+        try{
+            Files.walk(Paths.get(uploads))
+                    .filter()
+        }
+
+    }
 }
